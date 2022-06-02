ecfa5e38978f3345234fe3c930b1ea117645c5a4
AUGUSTO ITURRIAGA <106355876+TWELFarg@users.noreply.github.com>
2022-06-02 19:14:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 alert("BIENVENIDO A KURAMA  CLOTHING");
  let mensaje = confirm(" ESTA PAGINA CONTIENE ROPA MASCULINA!")
- if(mensaje) { 
-     alert(" DE EXELENTE CALIDAD ")
- }
- else {
-     alert( "CON EXELENTES PRECIOS")
- } 
